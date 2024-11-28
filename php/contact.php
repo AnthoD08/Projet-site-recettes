c087fc6d89a6bd2351298133529c95d351cd92ca
Anthony Delmotte <anthony-delmotte@hotmail.fr>
2024-11-28 14:57:35 +0100

    modif contact

--- php/contact.php
+++ php/contact.php
@@ -5,14 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="styles/styles.css" />
 </head>
 
 <?php
-include 'navbar.html';
+include('includes/navbar.html');
+
 ?>
 
 <body>
+    
+    <h2>Me contacter</h2>
 
 </body>
 
