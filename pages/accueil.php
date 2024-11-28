c6ecc932f070ffa14ab0ddcf9fee5265a2e87ed1
Anthony Delmotte <anthony-delmotte@hotmail.fr>
2024-11-28 10:28:28 +0100

    création du dossier contenant les pages du sites

---
+++ pages/accueil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+   
+<?php
+include ('includes/navbar.html')
+?>
+    <h1>Mon site de cuisine</h1>
+
+
+
+</body>
+
+</html>
