b9d28b314575506e749a0b9cae133afe6477792a
Anthony Delmotte <anthony-delmotte@hotmail.fr>
2024-12-03 08:36:28 +0100

    modif

--- php/accueil.php
+++ php/accueil.php
@@ -60,6 +60,7 @@
 
     ?>
 
+
     <?php
     include('includes/footer.html');
     ?>
--- php/toutes_les_recettes.php
+++ php/toutes_les_recettes.php
@@ -5,15 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="styles/styles.css" />
+    <base href="http://localhost/projets/Projet-site-recettes/">
+    <link rel="stylesheet"  href="styles/styles.css" />
 </head>
   <body>
 
   <?php include('includes/navbar.html'); ?>
 
-  
+    <h2>Toutes les recettes</h2>
 
-    <h1>Toutes les recettes</h1>
+
   </body>
 </html>
 
