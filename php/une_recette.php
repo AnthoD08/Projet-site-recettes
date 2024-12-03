f5dd2be08298c64207f44c0440648614adcde497
Anthony Delmotte <anthony-delmotte@hotmail.fr>
2024-12-03 10:49:28 +0100

    Ajout de la card liée à la recette entrée dans le formulaire

--- php/une_recette.php
+++ php/une_recette.php
@@ -5,12 +5,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css" />
 </head>
 
 <body>
 
-    <h1><?php echo ($nomRecette) ?></h1>
-    
+    <?php include('includes/navbar.html'); ?>
+
+    <?php
+    include './db/connexionBDD.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nom_recette = $_POST['nom_recette'];
+
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM recettes WHERE nom LIKE :nom");
+            $stmt->execute(['nom' => '%' . $nom_recette . '%']);
+            $recettes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($recettes) {
+                echo "<div class='recettes-container'>";
+                foreach ($recettes as $recette) {
+                    echo "<div class='recette-card'>";
+                    echo "<img src='" . htmlspecialchars($recette['image']) . "' alt='" . htmlspecialchars($recette['nom']) . "' class='recette-image'>";
+                    echo "<a href='ingredients.php?id=" . htmlspecialchars($recette['id_recette']) . "'>";
+                    echo "<h2 class='recette-title'>" . htmlspecialchars($recette['nom']) . "</h2>";
+                    echo "</a>";
+
+                    echo "</div>";
+                }
+                echo "</div>";
+            } else {
+                echo "<div class='recette'><p>Aucune recette trouvée.</p></div>";
+            }
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+        }
+    }
+    ?>
+
+
+
+
 </body>
 
 </html>
